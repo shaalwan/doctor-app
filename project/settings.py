33276c96be93fa2b680efcc2d1964cf3c1fce2bc
shubham salwan <ssalwan20@gmail.com>
2021-06-29 14:44:57 +0530

    'cors'

--- project/settings.py
+++ project/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'app',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
