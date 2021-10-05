83f4739e2378de9373d9fb675de3a84614bc74a1
shubham salwan <ssalwan20@gmail.com>
2021-10-05 20:42:12 +0530

    appointment

--- app/views.py
+++ app/views.py
@@ -424,7 +424,7 @@ class AppointmentViewset(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, requests):
+    def put(self, requests):
         appointment = Appointment.objects.get(pk=requests.data['id'])
         appointment.status = 1
         appointment.save()
